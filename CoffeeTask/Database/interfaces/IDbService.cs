c806e83261a83988d64c66d1aa546b1b714407e5
Wagner Christos <wagnerckv@gmail.com>
2023-12-26 10:35:32 -0300

    Delete CoffeeTask directory

--- CoffeeTask/Database/interfaces/IDbService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CoffeeTask.Database.interfaces
-{
-    public interface IDbService
-    {
-        Task<int> Create(string command, object parms);
-        Task<T?> GetOne<T>(string command, object parms);
-    }
-}
